6ad7889dc4b3cef5d221f8a4716722e5461fba1e
RajaParivesh <rajaparivesh@gmail.com>
2017-10-05 21:19:02 +0530

    database created in mlab.com and connected in our project (i.e. in config/db.js)

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const MongoClient  = require('mongodb').MongoClient;
 const bodyParser   = require('body-parser');
+const db             = require('./config/db');
 
 const app = express();
+const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// All route imported from routes folder
-require('./app/routes')(app, {});
 
-
-// Port Listing
-const port = 8000;
-app.listen(port, () => {
-  console.log('We are live on ' + port);
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+ 
+ // All route imported from routes folder
+  require('./app/routes')(app, database);
+ 
+ // Port Listing
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
 });
+
